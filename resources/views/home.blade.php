5f6d6aee629571c880ec9f1b44234e19e1f8eb7e
jmm <josemmazzei@gmail.com>
2020-06-01 10:58:14 -0300

    Update views
    Modify the home page structure.
    Add username field in register form.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <div class="row">
+    <div class="col-3 p-5">
+      <img src="http://lorempixel.com/160/160" alt="main image" class="rounded-circle">
+      
+  </div>
+  <div class="col-9">
+    <div class="pt-5">
+      <h1>{{ $user -> username }}</h1>
+    </div>    
+    <ul class="d-flex list-unstyled">
+      <li class="pr-3"><strong>0</strong> posts</li>
+      <li class="pr-3"><strong>0</strong> followers</li>
+      <li class="pr-3"><strong>0</strong> following</li>
+    </ul>
+    <div class="pt-2"><strong>{{ $user->profile->title }}</strong></div>
+    <div class="pt-2">{{ $user->profile->description }}</div>
+    <div class="pt-2"><a href="mysite.com">{{ $user->profile->link}}</a></div>
+  </div>
+ <div>
+   <section class="row pt-4">
+     <div class="col-4"><img src="http://lorempixel.com/500/600" alt="img1" class="w-100"></div>
+     <div class="col-4"><img src="http://lorempixel.com/500/600" alt="img2" class="w-100"></div>
+     <div class="col-4"><img src="http://lorempixel.com/500/600" alt="img3" class="w-100"></div>
+   </section>
+ </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+</div>  
 </div>
 @endsection
