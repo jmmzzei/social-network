f5f0baf3fe01795de84d5107ba5bcf2e714cc1a6
jmm <josemmazzei@gmail.com>
2020-06-02 10:58:29 -0300

    Add auth to post creation page

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class PostsController extends Controller
 {
+  public function __construct()
+  {
+   $this->middleware('auth');
+  }
+
 
   public function create(){
     return view('posts/create');
