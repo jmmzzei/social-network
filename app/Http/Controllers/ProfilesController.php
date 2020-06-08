0e90933010092c9241db01b5f6fefda43312f0fb
jmm <josemmazzei@gmail.com>
2020-06-08 09:22:31 -0300

    Add ProfilePolicy
    Restrict the profile edition and the creation of posts to the profile's owner.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,25 +4,40 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
   public function update(User $user){
+    $this->authorize('update', $user->profile);
     $data = request()->validate([
       'description'=>'required',
       'title'=>'required',
       'image'=>'',
       'link'=>'url',
     ]);
-   auth()->user()->profile->update($data);
-   return redirect("/profile/{$user->id}");
+  
+    if(request('image')){
+    $imagePath = request('image')->store('profile', 'public');
+
+    $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+    $image->save();
+    }
+  
+    auth()->user()->profile->update(array_merge(
+      $data,
+      ['image' => $imagePath],
+    ));
+    return redirect("/profile/{$user->id}");
   }
 
   public function edit(User $user){
+    $this->authorize('update', $user->profile);
     return view('profiles/edit', compact('user'));
   }
   
   public function index(User $user){
+    /* dd($user); */
     return view('profiles/index', compact('user'));
   }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,17 +9,23 @@
   <div class="col-9">
     <div class="pt-5 d-flex justify-content-between align-items-baseline">
       <h1>{{ $user -> username }}</h1>
+@can('update', $user->profile)
       <a href="/p/create">Add New Post</a>    
-  </div>    
-  <a href="/profile/{{$user->id}}/edit" class="mt-2 mb-2 d-block">Edit profile</a>
-  <ul class="d-flex list-unstyled">
+@endcan
+  </div>
+
+@can('update', $user->profile)
+    <a href="/profile/{{$user->id}}/edit" class="mt-2 mb-2 d-block">Edit profile</a>
+@endcan  
+
+<ul class="d-flex list-unstyled">
       <li class="pr-3"><strong>{{$user->posts->count()}}</strong> posts</li>
       <li class="pr-3"><strong>0</strong> followers</li>
       <li class="pr-3"><strong>0</strong> following</li>
     </ul>
-    <div class="pt-1"><strong>{{ $user->profile->title }}</strong></div>
-    <div class="pt-1">{{ $user->profile->description }}</div>
-    <div class="pt-1"><a href="mysite.com">{{ $user->profile->link}}</a></div>
+    <div class="pt-1"><strong>{{  $user->profile->title }}</strong></div>
+    <div class="pt-1">{{ $user->profile->description}}</div>
+    <div class="pt-1"><a href="mysite.com">{{ $user->profile->link }}</a></div>
   </div>
  <div>
    <section class="row pt-4">
@@ -27,12 +33,12 @@
       <div class="col-4 pb-4">
         <a href="/p/{{$post->id}}">
           <img src="/storage/{{$post->image}}" class="w-100">
-        </a>       
+        </a>
       </div>
-    @endforeach     
+    @endforeach
    </section>
  </div>
 
-</div>  
+</div>
 </div>
 @endsection
