cb78489520e395cede9b170558240ddce9075ffa
jmm <josemmazzei@gmail.com>
2020-06-08 10:14:56 -0300

    Edit post detail view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,10 +7,23 @@
       <div class="col-8">
         <img src="/storage/{{$post->image}}" class="w-100" >  
       </div>
-      <div class="col-4 border-left pl-4 pr-4">
-        <h3>{{$post->user->username}}</h3>      
-        <p>{{$post->caption}}</p>
-      </div>     
+      <div class="col-4 border-left pl-4 pr-5">
+       
+         <div class="d-flex align-items-center pb-4">
+          <div class="pr-3 ">
+            <img src="/storage/{{$post->user->profile->image}}" alt="profile image" class="rounded-circle w-100" style="max-width: 40px;">
+          </div>
+          <div class="font-weight-bold"> 
+            <a href="/profile/{{$post->user->id}}"> <span class="text-dark">{{$post->user->username}}</span></a>
+         <b class="p-2">·</b> 
+          <a href="#">Follow</a>          
+</div>                 
+        </div>
+        
+        <p><span class="font-weight-bold"> 
+          <a href="/profile/{{$post->user->id}}"> <span class="text-dark">{{$post->user->username}}</span></a>
+          </span> {{$post->caption}}</p>
+       </div>     
     </div>     
   </div>
   </div>
